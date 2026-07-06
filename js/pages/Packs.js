cc5ef92af84d05a8728599372123fa0fb21cf774
SGDList <sgdlist@gmail.com>
2026-07-06 13:44:01 +0200

    pack fix

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -54,7 +54,7 @@ export default {
                 </p>
             </div>
             <div class="level-container">
-                <div class="level" v-if="level || selected != null>
+                <div class="level" v-if="level || selected != null">
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
