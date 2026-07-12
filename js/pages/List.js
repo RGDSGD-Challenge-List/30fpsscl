93f6ccea317a84e505b9f59e1850c16a3cfa726a
GDNoob402 <gmd.noo8@gmail.com>
2026-07-13 09:27:05 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -1,4 +1,4 @@
-Pimport { store } from "../main.js";
+import { store } from "../main.js";
 import { embed } from "../util.js";
 import { score } from "../score.js";
 import { fetchChangelog, fetchEditors, fetchList } from "../content.js";
