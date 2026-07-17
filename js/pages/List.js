ef7cdcd76dc645f58b33b779c67c00e49b471dde
ballgoballing <157750718+ballgoballing@users.noreply.github.com>
2026-07-17 12:23:43 +0200

    1/100 chance the list turns purple

--- js/pages/List.js
+++ js/pages/List.js
@@ -219,6 +219,12 @@ export default {
         this.changelog = await fetchChangelog();
         this.leDaily = await fetchdailylul();
         console.log(this.leDaily);
+        if (Math.floor(Math.random() * 100) == 12 || JSON.parse(localStorage.getItem('purple'))) {
+            var r = document.querySelector(':root');
+            var rs = getComputedStyle(r);
+            r.style.setProperty('--color-primary', '#8800ff');
+            localStorage.setItem('purple', "true");
+        }
 
         // Error handling
         if (!this.list) {
